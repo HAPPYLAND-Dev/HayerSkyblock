ec0b592de0551d26acd8cf6d28695d758d08c267
xiaozhangup <xiaozhangshiw@163.com>
2022-11-09 16:55:36 +0800

    移除更新检查,本地化生物群系选择

---
+++ src/test/java/Runner.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Runner {
+
+    @Test
+    public void load() {
+        HashMap<String, Integer> list = new HashMap<>();
+        int i;
+        for (i = 1; i < 3000000; i++) {
+            list.put("Int" + i, 200);
+        }
+        for (i = 1; i < 40; i++) {
+            pick(list);
+        }
+    }
+
+    public static void pick(HashMap<String, Integer> list) {
+        long start = System.currentTimeMillis();
+        if (list.getOrDefault("Int2999000", 1) == 200) {
+            long now = System.currentTimeMillis();
+            System.out.println("Time: " + (now - start) + " ms");
+        }
+    }
+
+}
